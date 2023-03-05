2c64994ffc0c165453defc48cd8345fbcb5a3d9d
heyjyu <jiwoosmile@yonsei.ac.kr>
2023-03-06 05:34:18 +0900

    블로그 추가
    블로그 주소를 추가했다.

--- payload/etc.ts
+++ payload/etc.ts
@@ -8,8 +8,8 @@ const etc: IEtc.Payload = {
       title: '관련 수업 수강',
       subTitle:
         'Fortran으로 데이터를 다루는 법을 배우는 전자 계산 수업 및 리눅스 기본을 배우고 파이썬으로 3차원 데이터 처리를 다루는 방법을 배우는 기상프로그래밍 수업에서 A+ 획득',
-      startedAt: '2018-12',
-      endedAt: '2018-09',
+      startedAt: '2018-09',
+      endedAt: '2018-12',
     },
   ],
 };
