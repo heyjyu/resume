787d2a023ad4ef7bc5c4fe72918ad406339a435d
heyjyu <jiwoosmile@yonsei.ac.kr>
2023-03-07 06:07:33 +0900

    이력서 수정
    이력서를 수정하였다.

--- payload/skill.ts
+++ payload/skill.ts
@@ -5,23 +5,18 @@ const backend: ISkill.Skill = {
   items: [
     {
       title: 'JAVA',
-      level: 3,
     },
     {
       title: 'Spring boot',
-      level: 3,
     },
     {
       title: 'PHP',
-      level: 2,
     },
     {
       title: 'Python',
-      level: 2,
     },
     {
       title: 'JUnit',
-      level: 2,
     },
   ],
 };
@@ -31,15 +26,12 @@ const database: ISkill.Skill = {
   items: [
     {
       title: 'MySQL',
-      level: 2,
     },
     {
       title: 'PostgreSQL',
-      level: 2,
     },
     {
       title: 'H2',
-      level: 2,
     },
   ],
 };
@@ -49,55 +41,42 @@ const frontend: ISkill.Skill = {
   items: [
     {
       title: 'React.js',
-      level: 3,
     },
     {
       title: 'Vue.js',
-      level: 3,
     },
     {
       title: 'jquery',
-      level: 3,
     },
     {
       title: 'Cypress',
-      level: 3,
     },
     {
       title: 'Playwright',
-      level: 3,
     },
     {
       title: 'Codecept.js',
-      level: 3,
     },
     {
       title: 'Jest',
-      level: 3,
     },
     {
       title: 'Javascript',
-      level: 3,
     },
     {
       title: 'HTML/CSS',
-      level: 3,
     },
     {
       title: 'bootstrap',
-      level: 2,
     },
     {
       title: 'MobX',
-      level: 2,
     },
     {
       title: 'Recoil',
-      level: 2,
     },
     {
       title: 'Typescript',
-      level: 1,
     },
   ],
 };
